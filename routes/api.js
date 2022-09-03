53144d87f2609f02a53c7277d6a8ba84bb80f852
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-09-04 02:34:05 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -959,7 +959,7 @@ router.get('/randomgambar/indo', async (req, res, next) => {
 
 router.get('/randomgambar/jepang', async (req, res, next) => {
 
-	let jpn = await fetchJson('https://raw.githubusercontent.com/VarrelKun/data/main/jepang.json')
+	let jpn = await fetchJson('https://raw.githubusercontent.com/VarrelKun/data/main/jpn.json')
 	let random = jpn[Math.floor(Math.random() * jpn.length)]
 	var result = await getBuffer(random.result)
 	res.set({'Content-Type': 'image/jpg'})
