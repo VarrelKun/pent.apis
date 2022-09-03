a2f072746842d1580fb96f5f0a98e3bdd0e93aa4
GopalAsu <91899516+VarrelKun@users.noreply.github.com>
2022-09-03 23:06:55 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -950,7 +950,7 @@ router.get('/randomgambar/couplepp', async (req, res, next) => {
 router.get('/randomgambar/indo', async (req, res, next) => {
 
 	let indo = await fetchJson('https://raw.githubusercontent.com/VarrelKun/data/main/indonesia.json')
-	let random = indo[Math.floor(Math.random() * dadu.length)]
+	let random = indo[Math.floor(Math.random() * indo.length)]
 	var result = await getBuffer(random.result)
 	res.set({'Content-Type': 'image/jpg'})
 	res.send(result)
